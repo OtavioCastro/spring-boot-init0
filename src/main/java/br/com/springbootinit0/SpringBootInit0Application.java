22a1cde9736d455a420acdae530a838de8d88d0c
Otavio de Castro Venancio Neto <re037386@resource.com.br>
2019-12-11 16:26:31 -0300

    Commit inicial

---
+++ src/main/java/br/com/springbootinit0/SpringBootInit0Application.java
@@ -0,0 +1,13 @@
+package br.com.springbootinit0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInit0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInit0Application.class, args);
+	}
+
+}
